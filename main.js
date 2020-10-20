1268a1dc17c2cac861e9d6d02420b87186d93eb5
Cristian Francolino <cfrancolino@stechs.com.ar>
2020-10-20 14:01:51 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const fs = require('fs');
+
+const person = {
+    name: 'John Doe'
+};
+
+app.get('/', (req, res) => {
+    const data = {
+        person: person
+    };
+    ejs.renderFile('public/index.ejs', data, {}, (err, html) => {
+        res.send(html);
+    });
+})
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+})
